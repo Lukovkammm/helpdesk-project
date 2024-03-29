cfed220ea41fdfbf0dbd53da1a3e5266b438ad45
Maryna Snitko <maryna_snitko@epam.com>
2024-03-29 14:48:04 +0100

    feat: init project

---
+++ prettier.config.cjs
@@ -0,0 +1,11 @@
+const config = {
+  singleQuote: true,
+  endOfLine: 'auto',
+  semi: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  arrowParens: 'always',
+};
+
+module.exports = config;
